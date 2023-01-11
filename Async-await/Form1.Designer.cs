e457ba4c7a746d52f28eca097f5faed7ee1f0a79
uğur çinal <ugurcinal@outlook.com>
2023-01-11 10:36:46 +0300

    Add project files.

---
+++ Async-await/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace Async_await
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStep1 = new System.Windows.Forms.Button();
+            this.btnStep2 = new System.Windows.Forms.Button();
+            this.btnStep3 = new System.Windows.Forms.Button();
+            this.btnStep4 = new System.Windows.Forms.Button();
+            this.btnStep5 = new System.Windows.Forms.Button();
+            this.btnStep6 = new System.Windows.Forms.Button();
+            this.btnStep9 = new System.Windows.Forms.Button();
+            this.btnStep7 = new System.Windows.Forms.Button();
+            this.btnStep8 = new System.Windows.Forms.Button();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.lbTimer = new System.Windows.Forms.Label();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStep1
+            // 
+            this.btnStep1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep1.Location = new System.Drawing.Point(3, 3);
+            this.btnStep1.Name = "btnStep1";
+            this.btnStep1.Size = new System.Drawing.Size(204, 48);
+            this.btnStep1.TabIndex = 1;
+            this.btnStep1.Text = "Break eggs";
+            this.btnStep1.UseVisualStyleBackColor = true;
+            // 
+            // btnStep2
+            // 
+            this.btnStep2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep2.Location = new System.Drawing.Point(3, 57);
+            this.btnStep2.Name = "btnStep2";
+            this.btnStep2.Size = new System.Drawing.Size(204, 48);
+            this.btnStep2.TabIndex = 2;
+            this.btnStep2.Text = "Beat eggs";
+            this.btnStep2.UseVisualStyleBackColor = true;
+            // 
+            // btnStep3
+            // 
+            this.btnStep3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep3.Location = new System.Drawing.Point(6, 111);
+            this.btnStep3.Name = "btnStep3";
+            this.btnStep3.Size = new System.Drawing.Size(201, 48);
+            this.btnStep3.TabIndex = 3;
+            this.btnStep3.Text = "Add salt";
+            this.btnStep3.UseVisualStyleBackColor = true;
+            // 
+            // btnStep4
+            // 
+            this.btnStep4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep4.Location = new System.Drawing.Point(6, 165);
+            this.btnStep4.Name = "btnStep4";
+            this.btnStep4.Size = new System.Drawing.Size(201, 48);
+            this.btnStep4.TabIndex = 4;
+            this.btnStep4.Text = "Turn on stove";
+            this.btnStep4.UseVisualStyleBackColor = true;
+            // 
+            // btnStep5
+            // 
+            this.btnStep5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep5.Location = new System.Drawing.Point(6, 219);
+            this.btnStep5.Name = "btnStep5";
+            this.btnStep5.Size = new System.Drawing.Size(201, 48);
+            this.btnStep5.TabIndex = 5;
+            this.btnStep5.Text = "Heat cooker";
+            this.btnStep5.UseVisualStyleBackColor = true;
+            // 
+            // btnStep6
+            // 
+            this.btnStep6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep6.Location = new System.Drawing.Point(6, 273);
+            this.btnStep6.Name = "btnStep6";
+            this.btnStep6.Size = new System.Drawing.Size(201, 48);
+            this.btnStep6.TabIndex = 6;
+            this.btnStep6.Text = "Pour oil";
+            this.btnStep6.UseVisualStyleBackColor = true;
+            // 
+            // btnStep9
+            // 
+            this.btnStep9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep9.Location = new System.Drawing.Point(6, 435);
+            this.btnStep9.Name = "btnStep9";
+            this.btnStep9.Size = new System.Drawing.Size(201, 48);
+            this.btnStep9.TabIndex = 9;
+            this.btnStep9.Text = "Service";
+            this.btnStep9.UseVisualStyleBackColor = true;
+            // 
+            // btnStep7
+            // 
+            this.btnStep7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep7.Location = new System.Drawing.Point(6, 327);
+            this.btnStep7.Name = "btnStep7";
+            this.btnStep7.Size = new System.Drawing.Size(201, 48);
+            this.btnStep7.TabIndex = 7;
+            this.btnStep7.Text = "Add eggs";
+            this.btnStep7.UseVisualStyleBackColor = true;
+            // 
+            // btnStep8
+            // 
+            this.btnStep8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStep8.Location = new System.Drawing.Point(6, 381);
+            this.btnStep8.Name = "btnStep8";
+            this.btnStep8.Size = new System.Drawing.Size(201, 48);
+            this.btnStep8.TabIndex = 8;
+            this.btnStep8.Text = "Cook";
+            this.btnStep8.UseVisualStyleBackColor = true;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSync.Location = new System.Drawing.Point(203, 30);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(215, 65);
+            this.btnSync.TabIndex = 1;
+            this.btnSync.Text = "Start Cooking Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.AutoSize = true;
+            this.lbTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimer.Location = new System.Drawing.Point(694, 531);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(87, 32);
+            this.lbTimer.TabIndex = 4;
+            this.lbTimer.Text = "Timer";
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAsync.Location = new System.Drawing.Point(203, 116);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(215, 62);
+            this.btnAsync.TabIndex = 10;
+            this.btnAsync.Text = "Start Cooking Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Controls.Add(this.btnStep6);
+            this.pnlButtons.Controls.Add(this.btnStep2);
+            this.pnlButtons.Controls.Add(this.btnStep1);
+            this.pnlButtons.Controls.Add(this.btnStep3);
+            this.pnlButtons.Controls.Add(this.btnStep4);
+            this.pnlButtons.Controls.Add(this.btnStep5);
+            this.pnlButtons.Controls.Add(this.btnStep7);
+            this.pnlButtons.Controls.Add(this.btnStep9);
+            this.pnlButtons.Controls.Add(this.btnStep8);
+            this.pnlButtons.Location = new System.Drawing.Point(651, 12);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(207, 497);
+            this.pnlButtons.TabIndex = 11;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(3, 239);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(631, 324);
+            this.listBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(870, 572);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.lbTimer);
+            this.Controls.Add(this.btnSync);
+            this.Name = "Form1";
+            this.Text = "Async-await";
+            this.pnlButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+ //       private System.Windows.Forms.Button BreakEggs;
+        private System.Windows.Forms.Button btnStep2;
+        private System.Windows.Forms.Button btnStep3;
+        private System.Windows.Forms.Button btnStep4;
+        private System.Windows.Forms.Button btnStep5;
+        private System.Windows.Forms.Button btnStep6;
+        private System.Windows.Forms.Button btnStep9;
+        private System.Windows.Forms.Button btnStep7;
+        private System.Windows.Forms.Button btnStep8;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Label lbTimer;
+        private System.Windows.Forms.Button btnStep1;
+        private System.Windows.Forms.Button btnAsync;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Async-await/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async_await
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly System.Windows.Forms.Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += (sender, e) =>
+            {
+                lbTimer.Text = DateTime.Now.ToString("HH:mm:ss");
+            };
+
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            PrepareScreenForStarting();
+
+            var sw = Stopwatch.StartNew();
+
+            BreakEggs();
+            BeatEggs();
+            AddSalt();
+            TurnOnStove();
+            HeatCooker();
+            PourOil();
+            AddEggs();
+            Cook();
+            Service();
+
+            sw.Stop();
+            AddLog();
+            AddLog($"Toplam Yemek Pişirme Süresi: {sw.ElapsedMilliseconds:0} MS");
+        }
+
+        private async void btnAsync_Click(object sender, EventArgs e)
+        {
+            PrepareScreenForStarting();
+
+            var sw = Stopwatch.StartNew();
+
+
+            var eggTaskGroup = await BreakEggsAsync()
+                .ContinueWith(async _ =>
+                {
+                    await BeatEggsAsync();
+                    await AddSaltAsync();
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            var stoveTaskGroup = await TurnOnStoveAsync()
+                .ContinueWith(async _ =>
+                {
+                    await HeatCookerAsync();
+                    await PourOilAsync();
+                    await AddEggsAsync();
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            await Task.WhenAll(eggTaskGroup, stoveTaskGroup);
+
+
+            await CookAsync();
+            await ServiceAsync();
+
+
+            sw.Stop();
+            AddLog();
+            AddLog($"Toplam Yemek Pişirme Süresi: {sw.ElapsedMilliseconds:0} MS");
+        }
+        #region Sync Methods
+        public void BreakEggs()
+        {
+            Task.Delay(500).Wait();
+            AddLog("Yumurtalar kırıldı.");
+            Color1Buttons(1);
+        }
+        public void BeatEggs()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yumurtalar Çırpıldı");
+            Color1Buttons(2);
+        }
+        public void AddSalt()
+        {
+            Task.Delay(200).Wait();
+            AddLog("Tuz Eklendi");
+            Color1Buttons(3);
+        }
+        public void TurnOnStove()
+        {
+            Task.Delay(500).Wait();
+            AddLog("Ocak Açıldı");
+            Color1Buttons(4);
+        }
+        public void HeatCooker()
+        {
+            Task.Delay(1000).Wait();
+            AddLog("Tava Isındı");
+            Color1Buttons(5);
+        }
+        public void PourOil()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yağ Tavaya Döküldü");
+            Color1Buttons(6);
+        }
+        public void AddEggs()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yumurtalar Tavaya Döküldü");
+            Color1Buttons(7);
+        }
+        public void Cook()
+        {
+            Task.Delay(2000).Wait();
+            AddLog("Yumurtalar Pişti");
+            Color1Buttons(8);
+        }
+        public void Service()
+        {
+            Task.Delay(750).Wait();
+            AddLog("Yumurtalar Servis Edildi");
+            Color1Buttons(9);
+        }
+        #endregion
+
+        #region Async Methods
+
+        public async Task BreakEggsAsync()
+        {
+            await Task.Delay(500);
+            AddLog("Yumurtalar Kırıldı");
+            Color1Buttons(1);
+        }
+
+        public async Task BeatEggsAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yumurtalar Çırpıldı");
+            Color1Buttons(2);
+        }
+
+        public async Task AddSaltAsync()
+        {
+            await Task.Delay(200);
+            AddLog("Tuz Eklendi");
+            Color1Buttons(3);
+        }
+
+        public async Task TurnOnStoveAsync()
+        {
+            await Task.Delay(500);
+            AddLog("Ocak Açıldı");
+            Color2Buttons(4);
+        }
+
+        public async Task HeatCookerAsync()
+        {
+            await Task.Delay(1000);
+            AddLog("Tava Isındı");
+            Color2Buttons(5);
+        }
+
+        public async Task PourOilAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yağ Tavaya Döküldü");
+            Color2Buttons(6);
+        }
+
+        public async Task AddEggsAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yumurtalar Tavaya Döküldü");
+            Color2Buttons(7);
+        }
+
+        public async Task CookAsync()
+        {
+            await Task.Delay(200);
+            AddLog("Yumurtalar Pişti");
+            Color1Buttons(8);
+        }
+
+        public async Task ServiceAsync()
+        {
+            await Task.Delay(750);
+            AddLog("Yumurtalar Servis Edildi");
+            Color1Buttons(9);
+        }
+
+        #endregion
+
+        private void AddLog(string listStr = "")
+        {
+            if (!string.IsNullOrEmpty(listStr))
+                listStr = $"[{DateTime.Now:dd:MM.yyyy HH:mm:ss}] - {listStr}";
+
+            listBox1.Items.Add(listStr);
+            listBox1.TopIndex = listBox1.Items.Count - 1; // Locate the last row
+        }
+
+        private void PrepareScreenForStarting()
+        {
+            foreach (Control control in pnlButtons.Controls)
+            {
+                if (control is Button btn)
+                    btn.BackColor = SystemColors.Control;
+            }
+
+            pnlButtons.Update();
+
+            listBox1.Items.Clear();
+        }
+
+        private void Color1Buttons(int step)
+        {
+            Button btn = pnlButtons.Controls[$"btnStep{step}"] as Button;
+
+            btn.BackColor = Color.Yellow;
+        }
+
+        private void Color2Buttons(int step)
+        {
+            Button btn = pnlButtons.Controls[$"btnStep{step}"] as Button;
+
+            btn.BackColor = Color.DarkBlue;
+        }
+
+    }
+}
